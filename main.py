732740bbdd091eaecbc90f19b19b957402b4886c
mateimmo14 <mateimmo14@gmail.com>
2026-03-06 13:41:02 +0200

    Final commit of version v1.1, polished text, error handling, and added full compatibility of other domains

--- main.py
+++ main.py
@@ -3,11 +3,58 @@ from email.header import decode_header, make_header
 import email,threading,tkinter
 import smtplib
 import inquirer
+import bs4
 import ttkthemes,tkinter as tk
 from tkinter import scrolledtext
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+IMAP_SERVERS = {
+    "gmail.com": "imap.gmail.com",
+    "outlook.com": "imap-mail.outlook.com",
+    "hotmail.com": "imap-mail.outlook.com",
+    "live.com": "imap-mail.outlook.com",
+    "yahoo.com": "imap.mail.yahoo.com",
+    "yahoo.co.uk": "imap.mail.yahoo.com",
+    "icloud.com": "imap.mail.me.com",
+    "me.com": "imap.mail.me.com",
+    "aol.com": "imap.aol.com",
+    "protonmail.com": "imap.protonmail.com",
+    "zoho.com": "imap.zoho.com",
+    "gmx.com": "imap.gmx.com",
+    "gmx.net": "imap.gmx.net",
+    "gmx.de": "imap.gmx.net",
+    "mail.com": "imap.mail.com",
+    "yandex.com": "imap.yandex.com",
+    "yandex.ru": "imap.yandex.ru",
+    "fastmail.com": "imap.fastmail.com",
+    "fastmail.fm": "imap.fastmail.com",
+    "tutanota.com": "imap.tutanota.com",
+    "office365.com": "outlook.office365.com",
+}
 
+SMTP_SERVERS = {
+    "gmail.com": "smtp.gmail.com",
+    "outlook.com": "smtp-mail.outlook.com",
+    "hotmail.com": "smtp-mail.outlook.com",
+    "live.com": "smtp-mail.outlook.com",
+    "yahoo.com": "smtp.mail.yahoo.com",
+    "yahoo.co.uk": "smtp.mail.yahoo.com",
+    "icloud.com": "smtp.mail.me.com",
+    "me.com": "smtp.mail.me.com",
+    "aol.com": "smtp.aol.com",
+    "protonmail.com": "smtp.protonmail.com",
+    "zoho.com": "smtp.zoho.com",
+    "gmx.com": "mail.gmx.com",
+    "gmx.net": "mail.gmx.net",
+    "gmx.de": "mail.gmx.net",
+    "mail.com": "smtp.mail.com",
+    "yandex.com": "smtp.yandex.com",
+    "yandex.ru": "smtp.yandex.ru",
+    "fastmail.com": "smtp.fastmail.com",
+    "fastmail.fm": "smtp.fastmail.com",
+    "tutanota.com": "smtp.tutanota.com",
+    "office365.com": "smtp.office365.com",
+}
 import getpass
 def clear():
     if platform.system() == 'Windows':
@@ -20,13 +67,13 @@ def login():
     address = input("Please enter your email address:\n> ")
     password = getpass.getpass("Please enter your password:\n> ")
     try:
-        mail = imaplib.IMAP4_SSL(f"imap.{address.split("@")[1]}")
+        mail = imaplib.IMAP4_SSL(IMAP_SERVERS[address.split("@")[1]])
 
 
         mail.login(address,password)
     except (imaplib.IMAP4.error, smtplib.SMTPAuthenticationError) as e:
         clear()
-        if str(e) == b'[AUTHENTICATIONFAILED] Invalid credentials (Failure)':
+        if str(e) == '[AUTHENTICATIONFAILED] Invalid credentials (Failure)':
             print("Invalid credentials. Please try again.\nHint: If you have 2FA on your email account, go to \033]8;;https://myaccount.google.com/apppasswords\033\\\033[94mhttps://myaccount.google.com/apppasswords\033[0m\033]8;;\033\\ and create a app password and use it here to sign in")
             time.sleep(2)
         elif str(e) == "LOGIN command error: BAD [b'Not enough arguments provided 5c2dc8763939e-495dbdb4356mb40785970122']":
@@ -35,6 +82,9 @@ def login():
         else:
             print("Invalid credentials. Please try again.\nHint: If you have 2FA on your email account, go to \033]8;;https://myaccount.google.com/apppasswords\033\\\033[94mhttps://myaccount.google.com/apppasswords\033[0m\033]8;;\033\\ and create a app password and use it here to sign in")
             input("Press any key to continue...")
+        return login()
+    except KeyError:
+        input("Sorry, that domain isn't supported!\nPress any key to continue...")
         return login()
 
 def collect(amount):
@@ -83,18 +133,18 @@ def send_mail():
         clear()
         #-----enter text window----
 
-        cancelled = False
+        cancelled = [False]
 
         widget = ttkthemes.ThemedTk(theme="equilux")
         widget.geometry("700x500")
-        widget.title("Mail content")
+        widget.title("Mail Content")
         text = tk.scrolledtext.ScrolledText(widget)
         def rais():
             global cancelled
             clear()
             widget.destroy()
             input("Process aborted\nPress any key to continue...")
-            cancelled=True
+            cancelled=[True]
         def submit():
             msg["Subject"] = subject
             msg["From"] = fr
@@ -110,15 +160,15 @@ def send_mail():
 
         print("Submit the content to continue...")
         widget.mainloop()
-        if not cancelled:
+        if not cancelled[0]:
 
-            with smtplib.SMTP_SSL(f"smtp.{address.split("@")[1]}") as server:
+            with smtplib.SMTP_SSL(SMTP_SERVERS[address.split("@")[1]]) as server:
                 server.login(address, password)
                 server.sendmail(msg["From"], msg["To"], msg.as_string())
         clear()
     except Exception as e:
         clear()
-        input(f"{e}\nAn error occured\nPress any key to continue...")
+        input(f"{e}\nAn error occurred\nPress any key to continue...")
         return
 def spawn_text(text, title="popup"):
     max_width = 0
@@ -146,26 +196,35 @@ def view_emails():
         clear()
     except:
         clear()
-        input("An error occured\nPress any key to continue...")
+        input("An error occurred\nPress any key to continue...")
         return
 def view_email():
     try:
         clear()
-        email_id = int(input("Please enter the id of the email you wish to view:\n> "))
+        mail.select('inbox')
+        status, msg_data = mail.search(None, "ALL")
+        email_ids = msg_data[0].split()
+        email_id = int(input("Please enter the ID of the email you wish to view (Or enter -1 to view the latest email):\n> "))
 
         mail.select('inbox')
+        if email_id == -1:
+            email_id = len(email_ids)
         data = email.message_from_bytes(mail.fetch(str(email_id).encode(), '(RFC822)')[1][0][1])
         final = f"Subject: {data["Subject"]}\nFrom: {data['From']}\nTo: {data['To']}\n\n"
         if data.is_multipart():
             for part in data.walk():
                 if part.get_content_type() == "text/plain":
                     final += part.get_payload(decode=True).decode(part.get_content_charset() or "utf-8")
+                elif part.get_content_type() == "text/html":
+                    final += bs4.BeautifulSoup(part.get_payload(decode=True).decode(part.get_content_charset() or "utf-8"), "html.parser").get_text()
         else:
-            final += data.get_payload(decode=True).decode(data.get_content_charset() or "utf-8")
+
+                final += bs4.BeautifulSoup(data.get_payload(decode=True).decode(data.get_content_charset() or "utf-8"), "html.parser").get_text()
+
         spawn_text(final, "Email")
-    except:
+    except Exception as e:
         clear()
-        input("An error occured\nPress any key to continue...")
+        input(f"{e}\nAn error occurred\nPress any key to continue...")
         return
 def main():
     login()
